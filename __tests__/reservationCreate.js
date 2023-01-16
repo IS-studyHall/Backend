7eebbf890dc181f17497406a0a2b5883d8ec39a9
mikymatt <michele.mattiello2001@gmail.com>
2023-01-16 08:53:55 +0100

    fix and add reservation create test

--- __tests__/reservationCreate.js
+++ __tests__/reservationCreate.js
@@ -1,36 +1,54 @@
 test("reservation create - success", async () => {
+    const date = new Date();
     const data = {
-        date: '',
+        date: date.toUTCString(),
         start: '9:00',
         end: '11:00',
-        studyroom: '',
+        studyroom: '63c45cb6cb5f3d03f85cb5c8',
         user: '',
     }
+    const username = ''
+    const password = ''
+    //const {data} = await request.post('student/login', { username, password })
+    //request.defaults.headers.common.Authorization = data['data']['token']
+    //await request.get('') aggiungere route prenotazione
 });
 test("reservation create - error data non valida", async () => {
+    const date = new Date();
     const data = {
-        date: '',
+        date: date.toISOString(),
         start: '9:00',
         end: '11:00',
-        studyroom: '',
+        studyroom: '63c45cb6cb5f3d03f85cb5c8',
         user: '',
     }
+    //const {data} = await request.post('student/login', { username, password })
+    //request.defaults.headers.common.Authorization = data['data']['token']
+    //await request.get('') aggiungere route prenotazione e try catch per l errore
 });
 test("reservation create - error fascia oraria non valida", async () => {
+    const date = new Date();
     const data = {
-        date: '',
+        date: date.toUTCString(),
         start: '10:00',
         end: '11:00',
-        studyroom: '',
+        studyroom: '63c45cb6cb5f3d03f85cb5c8',
         user: '',
     }
+    //const {data} = await request.post('student/login', { username, password })
+    //request.defaults.headers.common.Authorization = data['data']['token']
+    //await request.get('') aggiungere route prenotazione e try catch per l errore
 });
 test("reservation create - errore giorno passato", async () => {
+    const date = new Date();
     const data = {
-        date: '',
-        start: '',
-        end: '',
-        studyroom: '',
+        date: date.toUTCString(),
+        start: '9:00',
+        end: '11:00',
+        studyroom: '63c45cb6cb5f3d03f85cb5c8',
         user: '',
     }
+    //const {data} = await request.post('student/login', { username, password })
+    //request.defaults.headers.common.Authorization = data['data']['token']
+    //await request.get('') aggiungere route prenotazione e try catch per l errore
 });
